8776aab1b39a12a4e7eee5442e85a7846d93f368
Px <guilhermeciotapx@outlook.com>
2024-11-19 03:15:39 -0300

    feat(goaws): string

---
+++ goaws/goaws.go
@@ -0,0 +1,5 @@
+package goaws
+
+func String(str string) *string {
+	return &str
+}
---
+++ goaws/goaws_test.go
@@ -0,0 +1,21 @@
+package goaws_test
+
+import (
+	"testing"
+
+	"github.com/itispx/goaws/goaws"
+)
+
+func TestString(t *testing.T) {
+	t.Parallel()
+
+	str := "string-test"
+
+	out := goaws.String(str)
+
+	if out == nil {
+		t.Error("expected non-nil pointer")
+	} else if *out != str {
+		t.Errorf("expected '%s', got '%s'", str, *out)
+	}
+}
